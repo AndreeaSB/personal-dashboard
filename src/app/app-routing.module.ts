035789e18a44aafcd6d007f3664425a8f1296ce8
AndreeaSB <andreea.sbadea@gmail.com>
2023-11-16 19:19:09 +0200

    bookmark model+service, todo pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TodosComponent } from './todos/todos.component';
 import { AddNoteComponent } from './add-note/add-note.component';
 import { EditNoteComponent } from './edit-note/edit-note.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { EditTodoComponent } from './edit-todo/edit-todo.component';
 
 const routes: Routes = [
   { path: 'bookmarks', component: BookmarksComponent, data:{ tab: 1} },
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'todos', component: TodosComponent, data:{ tab: 2} },
   { path: 'notes/add', component: AddNoteComponent},
   { path: 'notes/:id', component: EditNoteComponent},
-  { path: 'todos/add', component: AddTodoComponent }
+  { path: 'todos/add', component: AddTodoComponent },
+  { path: 'todos/:id', component: EditTodoComponent }
 ];
 
 @NgModule({
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../shared/todo.service';
 import { Todo } from '../shared/todo.model';
+import { Router } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
+  animations: [
+    trigger('todoItemAnim', [
+      transition(':leave', [
+        animate(1000, style({
+          opacity: 0,
+          height: 0,
+          marginBottom: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class TodosComponent implements OnInit {
   todos: Todo[];
   
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService, private router: Router) { }
 
   ngOnInit(): void {
     this.todos = this.todoService.getTodos();
@@ -18,5 +31,13 @@ export class TodosComponent implements OnInit {
 
   toggleCompleted(todo: Todo) {
     this.todoService.updateTodo(todo.id, {completed: !todo.completed})
+  }
+
+  onEditClick(todo: Todo) {
+    this.router.navigate(['/todos', todo.id]);
+  }
+
+  onDeleteClick(todo: Todo) {
+    this.todoService.deleteTodo(todo.id);
   }
 }
